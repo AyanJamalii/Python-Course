12053d6bad95d9dbc1e57e255b36efa723d9baa0
ibtasam-jamali <here.ayanjamali@gmail.com>
2026-08-26 22:28:41 +0500

    Prompts

---
+++ LangChain/langchain_components/Prompts/prompts_ui.py
@@ -0,0 +1,32 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import streamlit as st  
+import warnings
+
+
+warnings.filterwarnings("ignore")
+
+st.title("LangChain local prompt Demo.")
+
+
+@st.cache_resource
+
+def load_local_model():
+    llm = HuggingFacePipeline.from_model_id(
+        model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+        task="text-generation",
+        pipeline_kwargs={
+            "temperature" : 0.5,
+            "max_new_tokens" : 150
+        }
+    )
+    return ChatHuggingFace(llm=llm)
+
+model = load_local_model()
+
+user_prompt = st.text_input("Enter Prompt")
+
+if st.button("Generate Response"):
+    if user_prompt:
+        with st.spinner("processing locally...."):
+            response = model.invoke(user_prompt)
+            st.write(response.content)
