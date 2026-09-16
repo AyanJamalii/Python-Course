6407082e427c33bbda4d02833ee953b5c9a6b3c7
ibtasam-jamali <here.ayanjamali@gmail.com>
2026-09-16 19:15:31 +0500

    Wikipedia Retriver

--- LangChain/langchain_components/13. Retrievers/wikipedia_retriever.py
+++ LangChain/langchain_components/13. Retrievers/wikipedia_retriever.py
@@ -1,4 +1,35 @@
-# Starting Retreivers 
-# https://youtu.be/pJdMxwXBsk0?si=mhzmMB9OyCyiIhLF
+import os
+from dotenv import load_dotenv
+from langchain_community.retrievers import WikipediaRetriever
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-# Its been run on google colab :(
+load_dotenv()
+
+retriever = WikipediaRetriever(top_k_results=2)
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
+
+prompt = ChatPromptTemplate.from_template("""
+Answer the question based only on the following context:
+
+Context:
+{context}
+
+Question: {question}
+""")
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+response = chain.invoke("What is Quantum Computing?")
+print(response)
